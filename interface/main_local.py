4912d1e05a139b86ef88b1de7d1a5681f036df25
Amaury Canivet <canivetamaury@hotmail.com>
2023-02-18 16:41:58 +0100

    hello

--- interface/main_local.py
+++ interface/main_local.py
@@ -2,8 +2,13 @@
 import numpy as np
 import pandas as pd
 import os
+import pickle
 from ml_logic.data import clean_data
 from ml_logic.params import LOCAL_DATA_PATH
+from sklearn.model_selection import train_test_split
+from ml_logic.preprocessor import make_pipeline_fit_transform
+from ml_logic.model import define_train_model
+from ml_logic.params import filename
 
 def preprocess_and_train():
     # Load Datasets
@@ -14,8 +19,37 @@ def preprocess_and_train():
 
     #Return final Dataset
     data = clean_data(df_requests, df_bookings, df_users, df_logins)
-    return data
+    X_new = data.loc[[0]]
+    X_new = X_new.drop(columns=["booked"])
+    print(data.shape)
+    print(X_new)
+
+    # Balancing
+    #data =
+
+    # Create X, y
+    X = data.drop(columns=['booked'])
+    y = data['booked']
+
+
+    # Preprocess X using proprocessor.py
+    X_processed = make_pipeline_fit_transform(X)
+
+    # Train model on X_processed and y, using `model.py`
+    model = define_train_model(X_processed,y)
+    X_new_processed = make_pipeline_fit_transform(X_new)
+    loaded_model = pickle.load(open(filename,"rb"))
+    return model.predict(X_new_processed)
+
+    #return model
+
+#def predict(X_new):
+    X_new_processed = preprocess_features(X_new)
+    loaded_model = pickle.load(open(filename,"rb"))
+    return loaded_model.predict(X_new_processed)
+
+
 
 if __name__== "__main__":
-    df= preprocess_and_train()
-    print(df.head(5))
+    preprocess_and_train()
+    print("test")
