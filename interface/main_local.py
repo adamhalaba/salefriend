370a8fd3c0e585dd26e8683cbe2b57f01a7673f3
Amaury Canivet <canivetamaury@hotmail.com>
2023-02-18 11:57:04 +0100

    main

--- interface/main_local.py
+++ interface/main_local.py
@@ -6,10 +6,13 @@ from ml_logic.data import clean_data
 from ml_logic.params import LOCAL_DATA_PATH
 
 def preprocess_and_train():
+    # Load Datasets
     df_requests = pd.read_csv(os.path.join(LOCAL_DATA_PATH,"Requests.csv"))
     df_bookings = pd.read_csv(os.path.join(LOCAL_DATA_PATH,"Bookings.csv"))
     df_users = pd.read_csv(os.path.join(LOCAL_DATA_PATH,"Users.csv"))
     df_logins = pd.read_csv(os.path.join(LOCAL_DATA_PATH,"Logins.csv"))
+
+    #Return final Dataset
     data = clean_data(df_requests, df_bookings, df_users, df_logins)
     return data
 
